7fc3539a510677a8ae3424cf66c412aa258690c0
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-05-02 11:27:10 +0100

    Remove postChooser and simplify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
 import PostList from "./components/PostList";
-import PostChooser from "./components/PostChooser";
 
 function App() {
-  const [postNumber, setPostNumber] = useState(0)
-  return <div className="App">
-    <PostList postNumber={postNumber}/>
-    <PostChooser setPostNumber={setPostNumber}/>
-  </div>;
+  return (
+    <div className="App">
+      <PostList />
+    </div>
+  );
 }
 
 export default App;
